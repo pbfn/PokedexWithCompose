7a23712a11de7470399eefcdd65751e4e1c21207
Pedro Bruno <pbruno1283@gmail.com>
2022-08-30 15:06:16 -0300

    Search Component
    Criado o componente de pesquisa da tela inicial.

--- app/src/main/java/com/pedro_bruno/pokedexwithcompose/components/components.kt
+++ app/src/main/java/com/pedro_bruno/pokedexwithcompose/components/components.kt
@@ -3,23 +3,28 @@ package com.pedro_bruno.pokedexwithcompose.components
 import android.graphics.drawable.Icon
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 
-@Preview
 @Composable
 fun PokedexAppBar(
     title: String = "Pokédex",
@@ -47,7 +52,7 @@ fun PokedexAppBar(
                 }
 
                 if (showTitle) {
-                    Text(text = title)
+                    Text(text = title, style = TextStyle(fontSize = 24.sp))
                 } else {
                     Box {}
                 }
@@ -61,5 +66,50 @@ fun PokedexAppBar(
             }
         },
         elevation = 0.dp
+    )
+}
+
+@Composable
+fun SearchInput(
+    hint: String = "Search Pokemon",
+    modifier: Modifier,
+    valueState: MutableState<String>,
+    imeAction: ImeAction = ImeAction.Done,
+    onAction: KeyboardActions = KeyboardActions.Default
+) {
+    OutlinedTextField(
+        value = valueState.value,
+        onValueChange = { valueState.value = it },
+        modifier = Modifier.then(modifier),
+        label = {
+            Text(
+                text = hint,
+                style = TextStyle(color = Color(0xFFCCCCCC), fontSize = 16.sp)
+            )
+        },
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = "search",
+                tint = Color(0xFFCCCCCC),
+                modifier = Modifier
+                    .height(24.dp)
+                    .width(24.dp)
+            )
+        },
+        shape = RoundedCornerShape(20),
+        singleLine = true,
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            backgroundColor = Color(0xFFF6F6F6),
+            textColor = Color.Black,
+            focusedLabelColor = Color.Black,
+            focusedBorderColor = Color.Black,
+            unfocusedBorderColor = Color.Transparent,
+            cursorColor = Color.Black,
+            placeholderColor = Color.Black,
+            leadingIconColor = Color.Black
+        ),
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text, imeAction = imeAction),
+        keyboardActions = onAction
     )
 }
--- app/src/main/java/com/pedro_bruno/pokedexwithcompose/screens/home/HomeScreen.kt
+++ app/src/main/java/com/pedro_bruno/pokedexwithcompose/screens/home/HomeScreen.kt
@@ -1,25 +1,36 @@
 package com.pedro_bruno.pokedexwithcompose.screens.home
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.Button
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.pedro_bruno.pokedexwithcompose.components.PokedexAppBar
+import com.pedro_bruno.pokedexwithcompose.components.SearchInput
 import com.pedro_bruno.pokedexwithcompose.navigation.PokedexScreens
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun HomeScreen(navController: NavController) {
+
+    val searchInputState = rememberSaveable { mutableStateOf("") }
+    val keyboardController = LocalSoftwareKeyboardController.current
+
     Scaffold(topBar = {
         PokedexAppBar(
             backgroundColor = Color.White,
@@ -29,19 +40,35 @@ fun HomeScreen(navController: NavController) {
     }) {
         Surface(
             modifier = Modifier
-                .padding(8.dp)
-                .fillMaxSize()
+                .fillMaxSize(),
+            color = Color(0xFFF6F6F6)
         ) {
-            Row(
-                horizontalArrangement = Arrangement.Center,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Button(onClick = { navController.navigate(PokedexScreens.StatsScreen.name) }) {
-
+            Column() {
+                Surface(
+                    shape = RoundedCornerShape(
+                        bottomStartPercent = 20,
+                        bottomEndPercent = 20,
+                    ),
+                    color = Color.White
+                ) {
+                    SearchInput(
+                        valueState = searchInputState,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(top = 16.dp, start = 16.dp, end = 16.dp, bottom = 24.dp),
+                        onAction = KeyboardActions {
+                            onSearch(searchInputState.value.trim())
+                            keyboardController?.hide()
+                        }
+                    )
                 }
+
+
             }
-
         }
-
     }
+}
+
+fun onSearch(query: String) {
+    Log.d("onSearch", "onSearch: $query")
 }
